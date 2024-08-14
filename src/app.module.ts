c049ed2d46a32735cbaa2d5dc50b34051dd4073d
LucasFernandez111 <flucasyoel@outlook.es>
2024-08-14 00:36:03 -0300

    Changes in @module -Add service of Auth -Add config to eviroments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { GoogleAuthService } from './auth/google-auth/google-auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), AuthModule],
+  controllers: [AuthController],
+  providers: [GoogleAuthService],
 })
 export class AppModule {}
