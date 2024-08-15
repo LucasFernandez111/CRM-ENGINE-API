f4d52ca188abfdbf7d57443802ae7b49dd36d42d
Lucas <flucasyoel@gmail.com>
2024-08-15 15:21:06 -0300

    feat: :sparkles:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET as string;
 
@@ -7,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  app.use(cookieParser(COOKIE_SECRET));
 
   await app.listen(3000);
 }
