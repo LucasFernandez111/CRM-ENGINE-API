9d3f4e5362baec574eb6dd48e0d56cdcfc14f974
Lucas <flucasyoel@gmail.com>
2024-08-15 15:25:59 -0300

    Add imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { GoogleAuthService } from './google-auth/google-auth.service';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { GoogleOauthGuard } from './guards/google-oauth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [GoogleAuthService, GoogleStrategy],
-  exports: [GoogleAuthService],
+  providers: [GoogleAuthService, GoogleStrategy, GoogleOauthGuard],
+  exports: [GoogleAuthService, GoogleOauthGuard],
 })
 export class AuthModule {}
