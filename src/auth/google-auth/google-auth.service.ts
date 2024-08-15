a196347c00ed8a438d529ed20d7e8b2aba2ce634
LucasFernandez111 <flucasyoel@outlook.es>
2024-08-15 00:46:54 -0300

    refactor: :fire:

--- src/auth/google-auth/google-auth.service.ts
+++ src/auth/google-auth/google-auth.service.ts
@@ -1,30 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { OAuth2Client } from 'google-auth-library';
-import { TokenResponse } from 'google-auth-library/build/src/auth/impersonated';
-import { GetTokenResponse } from 'google-auth-library/build/src/auth/oauth2client';
 
 @Injectable()
-export class GoogleAuthService {
-  private oauth2Client: OAuth2Client;
-  constructor() {
-    this.oauth2Client = new OAuth2Client({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirectUri: process.env.GOOGLE_REDIRECT_URI as string,
-    });
-  }
-
-  getAuthUrl(): string {
-    const authUrl: string = this.oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['https://www.googleapis.com/auth/userinfo.profile'],
-    });
-    return authUrl;
-  }
-
-  async getTokens(code: string) {
-    const tokens: GetTokenResponse = await this.oauth2Client.getToken(code);
-
-    return tokens;
-  }
-}
+export class GoogleAuthService {}
