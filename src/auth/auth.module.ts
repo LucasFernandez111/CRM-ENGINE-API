aeb8de8ccc24f18918d7321e64f0aebe73b2f32b
LucasFernandez111 <flucasyoel@outlook.es>
2024-08-15 00:45:49 -0300

    Config module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { GoogleAuthService } from './google-auth/google-auth.service';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [GoogleAuthService],
+  providers: [GoogleAuthService, GoogleStrategy],
+  exports: [GoogleAuthService],
 })
 export class AuthModule {}
