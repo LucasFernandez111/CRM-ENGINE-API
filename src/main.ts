53d88be45023753a6e1d1b01765a84663a9ff6fc
LucasFernandez111 <flucasyoel@outlook.es>
2024-08-15 00:40:56 -0300

    feat:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const COOKIE_SECRET = process.env.COOKIE_SECRET as string;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
