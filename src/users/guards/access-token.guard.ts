450bfdadedfd8cd7a222985137ba3502f8555ab3
Lucas <flucasyoel@gmail.com>
2024-08-21 17:51:23 -0300

    fix: :recycle: Change throw Error

--- src/users/guards/access-token.guard.ts
+++ src/users/guards/access-token.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users.service';
+
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
   constructor(private readonly usersService: UsersService) {}
@@ -8,9 +14,11 @@ export class AccessTokenGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const accessToken = request.cookies['access_token'];
 
-    if (!accessToken) throw new Error('No access token found.');
+    if (!accessToken) {
+      throw new UnauthorizedException('No access token found.');
+    }
 
-    const authClient = this.usersService.authClient;
+    const authClient = this.usersService.oauth2Client;
 
     try {
       authClient.setCredentials({ access_token: accessToken });
@@ -18,14 +26,21 @@ export class AccessTokenGuard implements CanActivate {
       const tokenInfo = await authClient.getTokenInfo(accessToken);
       if (tokenInfo.expiry_date && tokenInfo.expiry_date < Date.now()) {
         console.log('Access token has expired. Refreshing token...');
+
         const tokens = await authClient.refreshAccessToken();
         authClient.setCredentials(tokens.credentials);
+
+        request.cookies['access_token'] = tokens.credentials.access_token;
       }
+
+      request.user = {
+        accessToken: authClient.credentials.access_token,
+      };
 
       return true;
     } catch (error) {
       console.error('Error validating access token:', error.message);
-      return false;
+      throw new UnauthorizedException('Invalid or expired access token.');
     }
   }
 }
