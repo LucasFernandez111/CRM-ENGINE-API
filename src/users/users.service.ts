47cc48853469cbf578897121c124b277f239d3d2
Lucas <flucasyoel@gmail.com>
2024-08-21 17:53:58 -0300

    refactor: :recycle:

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import { google, sheets_v4 } from 'googleapis';
+import {
+  HttpCode,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { google } from 'googleapis';
+import { oauth2 } from 'googleapis/build/src/apis/oauth2';
 
 @Injectable()
 export class UsersService {
-  private sheets: sheets_v4.Sheets;
-  public authClient;
+  public oauth2Client;
 
   constructor() {
-    this.sheets = google.sheets({ version: 'v4' });
-    this.authClient = new google.auth.OAuth2();
+    this.oauth2Client = new google.auth.OAuth2();
+  }
+
+  async getProfile() {
+    const { data } = await google.oauth2('v2').userinfo.get({
+      auth: this.oauth2Client,
+    });
+
+    return data;
   }
 
   async getRows(spreadSheetId: string) {
-    const response = await this.sheets.spreadsheets.values.get({
-      auth: this.authClient,
+    const response = await google.sheets('v4').spreadsheets.values.get({
+      auth: this.oauth2Client,
       spreadsheetId: spreadSheetId,
       range: 'A:D',
     });
 
     const rows = response.data.values;
     return rows;
+  }
+
+  async updateRow(spreadSheetId: string, range: string, values: string[]) {
+    if (!range || !values || !spreadSheetId)
+      return new NotFoundException('Range or values not provided');
+
+    if (values.length > 4)
+      return new NotFoundException(`Range too long ${range.length}`);
+    try {
+      await google.sheets('v4').spreadsheets.values.update({
+        auth: this.oauth2Client,
+        spreadsheetId: spreadSheetId,
+        range: range,
+        valueInputOption: 'RAW',
+        requestBody: {
+          values: [values],
+        },
+      });
+    } catch (error) {
+      return new InternalServerErrorException(error);
+    }
   }
 }
