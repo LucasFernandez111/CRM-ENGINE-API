31893ffab68aa2fae23d0b4058a010a42be08178
Lucas <flucasyoel@gmail.com>
2024-08-21 17:52:40 -0300

    fix: :bug: Fix problem with cors
    Accept credentials and change origin of frond end

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 const COOKIE_SECRET = process.env.COOKIE_SECRET as string;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser(COOKIE_SECRET));
+  app.enableCors({
+    origin: 'http://localhost:3038',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
 
   await app.listen(3000);
 }
