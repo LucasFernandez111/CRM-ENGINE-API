cdb07b453ab4e7a1141a79d9181de4e83132a0ce
Lucas <flucasyoel@gmail.com>
2024-08-22 15:42:01 -0300

    imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { AuthModule } from 'src/auth/auth.module';
 import { UsersService } from './users.service';
+import { UsersDbService } from './users-db.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersSchema } from 'src/schemas/users.schema';
 
 @Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      {
+        name: 'Users',
+        schema: UsersSchema,
+      },
+    ]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersDbService],
 })
 export class UsersModule {}
