817a54f23ddf4c47bf6ba7c1b2d7f9f0dab9f2ab
Lucas <flucasyoel@gmail.com>
2024-08-23 22:27:31 -0300

    refactor: :safety_vest: Add field sheet

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UsersDto {
   @IsNotEmpty()
@@ -11,4 +11,8 @@ export class UsersDto {
   @IsNotEmpty()
   @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
+  sheet?: string;
 }
