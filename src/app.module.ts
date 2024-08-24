7548f4470b8612fb47410e25a107ea521a638039
Lucas <flucasyoel@gmail.com>
2024-08-23 22:10:38 -0300

    Import OrdersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
     MongooseModule.forRoot(process.env.DB_CONNECT),
     AuthModule,
     UsersModule,
+    OrdersModule,
   ],
 })
 export class AppModule {}
