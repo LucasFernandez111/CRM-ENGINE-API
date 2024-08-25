3f03cd84247797478d869f35b298d0d8570cd6e2
Lucas <flucasyoel@gmail.com>
2024-08-25 18:41:05 -0300

    feat: :sparkles: Creation endpoint for Orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,18 +1,41 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+
+import { Request } from 'express';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Get()
-  getOrders() {
-    return 'orders';
+  async getOrders(
+    @Req() req: Request,
+    @Query('id_token') idtokenQuery: string,
+  ) {
+    const id_token = req.cookies['id_token'] || idtokenQuery;
+
+    const orders = await this.ordersService.getOrders(id_token);
+
+    return orders;
   }
   @Get(':id')
-  getOrder() {
-    return 'order';
+  async getOrder(@Param('id') order: string) {
+    const id_token = 'g34mg23323f3434e342453ef3533434g';
+
+    const orderFound = this.ordersService.getOrder(id_token, order);
+
+    return orderFound;
   }
 
   @Post()
@@ -29,12 +52,11 @@ export class OrdersController {
   }
 
   @Delete(':id')
-  deleteOrder() {
-    return 'delete order';
-  }
+  async deleteOrder(@Param('id') order: string) {
+    const id_token = 'g34mg23323f3434e342453ef3533434g';
 
-  @Get('/users/:userId/orders')
-  getUserOrders() {
-    return 'user orders';
+    const orderDeleted = await this.ordersService.deleteOrder(id_token, order);
+
+    return orderDeleted;
   }
 }
