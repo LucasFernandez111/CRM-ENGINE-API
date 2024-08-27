ff301b9cd65dc25d3ef54a05bf8058a9bf18a018
Lucas <flucasyoel@gmail.com>
2024-08-27 20:58:38 -0300

    feat(Controller): :sparkles: Add functionality for save user in db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Res, Req, UseGuards } from '@nestjs/common';
-import { GoogleAuthService } from './google-auth/google-auth.service';
 import { GoogleOauthGuard } from './guards/google-oauth.guard';
 import { Response } from 'express';
+import { DatabaseUsersService } from 'src/database/database-users/database-users.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly googleAuthService: GoogleAuthService) {}
+  constructor(private readonly DatabaseUsersService: DatabaseUsersService) {}
 
   @Get('google')
   @UseGuards(GoogleOauthGuard)
@@ -13,9 +13,13 @@ export class AuthController {
 
   @Get('google/callback')
   @UseGuards(GoogleOauthGuard)
-  googleAuthCallback(@Req() req, @Res() res: Response) {
+  async googleAuthCallback(@Req() req, @Res() res: Response) {
     // Aquí puedes manejar el resultado de la autenticación
     const { accessToken, id_token } = req.user;
+
+    const user = await this.DatabaseUsersService.getUser(id_token);
+
+    if (!user) await this.DatabaseUsersService.createUser(req.user);
 
     res.cookie('access_token', accessToken, {
       httpOnly: true,
@@ -31,7 +35,7 @@ export class AuthController {
     });
 
     return res.redirect(
-      `http://localhost:3038/auth/callback?token=${accessToken}`,
+      `${process.env.CLIENT_URI}/auth/callback?token=${accessToken}`,
     );
   }
 }
