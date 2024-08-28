f5bca6746ab3ded89c0c51f3581814a7b37d08af
Lucas <flucasyoel@gmail.com>
2024-08-28 13:39:30 -0300

    feat: Nuevo servicio para el manejo y obtencion de datos del usuario

---
+++ src/database/database-users/database-users.service.ts
@@ -0,0 +1,51 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/common/dto/create-user.dto';
+import { Users } from 'src/schemas/users.schema';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+
+@Injectable()
+export class DatabaseUsersService {
+  constructor(@InjectModel('Users') private readonly userModel: Model<Users>) {}
+
+  async createUser(user: CreateUserDto): Promise<Users> {
+    try {
+      const userCreated = await this.userModel.create(user);
+      return userCreated;
+    } catch (error) {
+      throw new InternalServerErrorException({
+        message: 'Error creating user',
+        error: error.message,
+      });
+    }
+  }
+
+  async getUser(id_token: string) {
+    if (!id_token) throw new BadRequestException('id_token is required');
+    try {
+      const user = await this.userModel.findOne({ id_token });
+      return user;
+    } catch (error) {
+      throw new NotFoundException(error?.message);
+    }
+  }
+
+  async updateUser(id_token: string, userData: UpdateUserDto) {
+    if (!id_token) throw new BadRequestException('id_token is required');
+    try {
+      const user = await this.userModel.findOneAndUpdate(
+        { id_token },
+        userData,
+      );
+      return user;
+    } catch (error) {
+      throw new NotFoundException(error?.message);
+    }
+  }
+}
