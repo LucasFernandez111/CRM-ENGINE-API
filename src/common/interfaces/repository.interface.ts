ab516a1b2e8e1e3245ab0a77eb952322da2af68e
LucasFernandez111 <flucasyoel@outlook.es>
2024-09-14 19:19:53 -0300

    feat(repository): create interface for generic repository

---
+++ src/common/interfaces/repository.interface.ts
@@ -0,0 +1,10 @@
+/**
+ * Interface generic repository
+ */
+export interface IRepository<T> {
+  create(entity: T): Promise<T>;
+  findById(id: string): Promise<T | null>;
+  findAll(): Promise<T[]>;
+  update(id: string, entity: Partial<T>): Promise<T | null>;
+  delete(id: string): Promise<void>;
+}
