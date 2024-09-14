5c5d9a3ab185a61cecf4c76383ac7bc4fa76791b
LucasFernandez111 <flucasyoel@outlook.es>
2024-09-14 19:20:43 -0300

    refactor(module): move new module to providers

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -4,11 +4,12 @@ import { OrdersService } from './orders.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderSchema } from '../schemas/orders.schema';
 import { UsersModule } from '../users/user.module';
+import { OrderRepository } from './order.repository';
 
 @Module({
   imports: [UsersModule, MongooseModule.forFeature([{ name: 'Order', schema: OrderSchema }])],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderRepository],
   exports: [OrdersService],
 })
 export class OrdersModule {}
