c76c7bd420fad3f0f23a754a385febc301463eb6
Lucas <flucasyoel@gmail.com>
2024-09-18 11:21:29 -0300

    ✨ feat(service): add DateFilterService with date range utilities
    - Added functions to get first and last dates for day, week, month, and year.
    - Added utility functions to handle start and end of day in UTC.
    - Implemented helper methods for formatting dates and retrieving all months in a year.

--- src/modules/orders/services/date-filter/date-filter.service.ts
+++ src/modules/orders/services/date-filter/date-filter.service.ts
@@ -1,6 +1,50 @@
 import { Injectable } from '@nestjs/common';
-import { Order } from 'src/schemas/orders.schema';
-import { OrderRepository } from '../order-repository/order-repository.service';
 
 @Injectable()
-export class DateFilterService {}
+export class DateFilterService {
+  constructor() {}
+
+  public getMonths(date: Date): Date[] {
+    return Array.from({ length: 12 }, (_, i) => new Date(Date.UTC(date.getFullYear(), i, 1)));
+  }
+
+  public getFirstDateOfDay(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+  }
+
+  public getLastDateOfDay(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59, 999));
+  }
+  public getFirstDateOfMonth(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), 1));
+  }
+  public getLastDateOfMonth(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0, 23, 59, 59, 999));
+  }
+
+  public getFirstDateOfWeek(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 1));
+  }
+  public getLastDateOfWeek(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 7, 23, 59, 59, 999));
+  }
+
+  public getFirstDateOfYear(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), 0, 1));
+  }
+  public getLastDateOfYear(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), 11, 31, 23, 59, 59, 999));
+  }
+
+  public setStartOfDateUTC(date: Date): Date {
+    return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 0, 0, 0));
+  }
+
+  public setEndOfDateUTC(date: Date): Date {
+    return new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), 23, 59, 59));
+  }
+
+  public toString(date: Date): string {
+    return date.toISOString().split('T')[0];
+  }
+}
