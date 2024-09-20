ff8dd351a6f7af8715ac90936a7c6eed3ecf0abd
Lucas <flucasyoel@gmail.com>
2024-09-20 01:55:03 -0300

    ✨ feat(service): implement ProductsService with CRUD operations
    - Added createProduct method to handle product creation.
    - Implemented updateProduct method to update product details.
    - Added deleteProduct method to remove a product by ID.
    - Included findAllProducts method to retrieve all products for a user.

---
+++ src/modules/products/services/products.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsRepository } from './products-repository/products-repository.service';
+import { ErrorManager } from 'src/config/error.manager';
+import { ProductCreateDTO } from '../interfaces/product-create.dto';
+import { Product } from 'src/schemas/products.schema';
+import { ProductUpdateDTO } from '../interfaces/product-update.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly productsRepository: ProductsRepository) {}
+
+  public async createProduct(userId: string, product: ProductCreateDTO): Promise<Product> {
+    try {
+      return await this.productsRepository.create({ ...product, userId });
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async updateProduct(id: string, product: ProductUpdateDTO): Promise<Product> {
+    try {
+      return await this.productsRepository.update(id, product);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+  public async deleteProduct(id: string): Promise<void> {
+    try {
+      return await this.productsRepository.delete(id);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+
+  public async findAllProducts(userId: string): Promise<Product[]> {
+    try {
+      const products = await this.productsRepository.findAllBy(userId);
+      if (!products || !products.length) {
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'Products not found',
+        });
+      }
+      return products;
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+}
