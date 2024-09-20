4944fd8115b00467326bd32a093618c2b6d0fb81
Lucas <flucasyoel@gmail.com>
2024-09-19 23:41:40 -0300

    refactor(Module): :recycle: Changes string name in the import by Mongoose Module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { OrderSchema } from '../../schemas/orders.schema';
+import { Order, OrderSchema } from '../../schemas/orders.schema';
 import { UsersModule } from '../users/user.module';
 import { OrderRepository } from './services/order-repository/order-repository.service';
-import { DateModule } from 'src/date/date.module';
 import { SalesStatisticsService } from './services/sales-statistics/sales-statistics.service';
 import { OrdersService } from './services/orders.service';
 import { DateFilterService } from './services/date-filter/date-filter.service';
 import { OrdersStatisticsController } from './controllers/orders-statistics.controller';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forFeature([{ name: 'Order', schema: OrderSchema }]), DateModule],
+  imports: [UsersModule, MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])],
   controllers: [OrdersController, OrdersStatisticsController],
   providers: [OrdersService, OrderRepository, DateFilterService, SalesStatisticsService],
   exports: [OrdersService],
