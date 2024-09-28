092cfe7f57cce5d5ae1029130ab5d9fdac0f1f0d
Lucas <flucasyoel@gmail.com>
2024-09-28 19:51:19 -0300

    refactor: :fire:

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { GoogleAuthService } from './google-auth/google-auth.service';
 import { GoogleOauthGuard } from './guards/google-oauth.guard';
-import { AccessTokenGuard } from './guards/access-token.guard';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UsersModule } from '../users/user.module';
 import { AuthService } from './services/auth.service';
@@ -10,7 +9,7 @@ import { AuthService } from './services/auth.service';
 @Module({
   imports: [forwardRef(() => UsersModule)],
   controllers: [AuthController],
-  providers: [GoogleAuthService, GoogleStrategy, GoogleOauthGuard, AccessTokenGuard, AuthService],
-  exports: [GoogleAuthService, GoogleOauthGuard, AccessTokenGuard],
+  providers: [GoogleAuthService, GoogleStrategy, GoogleOauthGuard, , AuthService],
+  exports: [GoogleAuthService, GoogleOauthGuard],
 })
 export class AuthModule {}
