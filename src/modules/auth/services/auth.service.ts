573ff200424ecde2606d619110112022123ed1fb
Lucas <flucasyoel@gmail.com>
2024-09-28 19:51:40 -0300

    refactor: :fire:

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/schemas';
 import * as jwt from 'jsonwebtoken';
+import ErrorManager from 'src/config/error.manager';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +15,6 @@ export class AuthService {
   public async signJWT(user: Partial<User>) {
     const payload = {
       sub: user.id_token,
-      refreshToken: user.refresh_token,
     };
 
     return await this.generateJWT({
@@ -24,6 +24,14 @@ export class AuthService {
     });
   }
 
+  public async verifyJWT(token: string): Promise<jwt.JwtPayload | string> {
+    try {
+      await jwt.verify(token, process.env.JWT_SECRET);
+      return await jwt.decode(token);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
   private async generateJWT({
     payload,
     secret,
