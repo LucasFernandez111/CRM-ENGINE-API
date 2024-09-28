2b5c6ec13c45ae1985ae8796fd3a69408821d269
Lucas <flucasyoel@gmail.com>
2024-09-28 19:52:12 -0300

    refactor: :fire:

--- src/modules/sheets/controllers/sheets/sheets.controller.ts
+++ src/modules/sheets/controllers/sheets/sheets.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { SheetService } from '../../services/sheets/sheet.service';
-import { AccessTokenGuard } from 'src/modules/auth/guards/access-token.guard';
 import { Request } from 'express';
 
 @Controller('sheets')
@@ -8,7 +7,6 @@ export class SheetsController {
   constructor(private readonly sheetService: SheetService) {}
 
   @Get('products')
-  @UseGuards(AccessTokenGuard)
   async getProducts(@Req() req: Request) {
     const id_token = req.cookies['id_token'];
     const access_token = req.cookies['access_token'];
