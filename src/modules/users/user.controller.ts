be4017986a20e396488adac9d4686201ddb9afb0
Lucas <flucasyoel@gmail.com>
2024-10-04 19:26:10 -0300

    feat(Controller): :sparkles: Creation endpoint for get users

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Put, Body, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express'; // Asegúrate de importar Request
 import { UsersService } from './services/user.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth-guard/jwt-auth.guard';
+import { UserExistsGuard } from './guards/user-exists.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @UseGuards(JwtAuthGuard, UserExistsGuard)
+  async getUser(@Req() req) {
+    const id_token = req.user.sub;
+
+    return await this.usersService.findUserByTokenId(id_token);
+  }
 }
