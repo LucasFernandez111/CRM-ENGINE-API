f859d8512a54dca9907a40e997edca938408f118
Lucas <flucasyoel@gmail.com>
2024-10-04 19:10:36 -0300

    refactor(main) ♻ Change the name of the  environment variable

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CLIENT_URI as string,
+    origin: process.env.ORIGIN_URI as string,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
