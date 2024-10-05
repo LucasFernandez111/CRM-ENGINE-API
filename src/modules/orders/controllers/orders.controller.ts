52a739f361603eb235362953d1debc57b080a153
LucasFernandez111 <flucasyoel@outlook.es>
2024-10-05 09:29:54 -0300

    refactor(Controller): :fire: Remove code  unused

--- src/modules/orders/controllers/orders.controller.ts
+++ src/modules/orders/controllers/orders.controller.ts
@@ -13,9 +13,7 @@ export class OrdersController {
 
   @Post()
   @UseGuards(JwtAuthGuard, UserExistsGuard)
-  async createOrder(@Req() req, @Body() order: CreateOrderDto): Promise<Order> {
-    console.log(order);
-
+  async createOrder(@Req() req, @Body() order: CreateOrderDto): Promise<any> {
     const { sub: userId }: PayloadToken = req.user;
 
     return this.ordersService.createOrder(userId, order);
@@ -37,100 +35,4 @@ export class OrdersController {
   async updateOrder(@Param('id') id: string, @Body() order: UpdateOrderDto): Promise<Order> {
     return await this.ordersService.updateOrder(id, order);
   }
-
-  // /**
-  //  * Obtener órdenes basadas en un rango de fechas.
-  //  *
-  //  * @param startDate - La fecha de inicio del rango (formato YYYY-MM-DD).
-  //  * @param endDate - La fecha de fin del rango (formato YYYY-MM-DD).
-  //  * @param req - El objeto de solicitud HTTP que contiene las cookies.
-  //  * @returns Una lista de órdenes que fueron creadas dentro del rango de fechas especificado.
-  //  *
-  //  * Este endpoint devuelve órdenes creadas dentro del rango de fechas especificado.
-  //  * El `id_token` se extrae de las cookies para autenticar la solicitud.
-  //  * Las fechas deben proporcionarse en formato `YYYY-MM-DD`.
-  //  */
-  // // @Get('range')
-  // // async getRecordsByDateRange(
-  // //   @Query('startDate') startDate: string,
-  // //   @Query('endDate') endDate: string,
-  // //   @Req() req: Request,
-  // // ): Promise<Order[] | []> {
-  // //   const id_token = req.cookies['id_token'];
-
-  // //   const startDateRange = new Date(startDate);
-  // //   const endDateRange = new Date(endDate);
-
-  // //   startDateRange.setUTCHours(0, 0, 0, 0);
-  // //   endDateRange.setUTCHours(23, 59, 59, 999);
-
-  // //   const records = await this.ordersService.getRecordsByDateRange(id_token, startDateRange, endDateRange);
-  // //   return records;
-  // // }
-
-  // /**
-  //  * Obtener todas las órdenes asociadas con el usuario autenticado.
-  //  *
-  //  * @param req - El objeto de solicitud HTTP que contiene las cookies.
-  //  * @returns Una lista de órdenes asociadas con el `id_token` proporcionado.
-  //  *
-  //  * Este endpoint devuelve todas las órdenes asociadas con el usuario autenticado.
-  //  * El `id_token` se extrae de las cookies o del parámetro de consulta como alternativa.
-  //  */
-  // @Get()
-  // async getOrders(@Req() req: Request): Promise<Order[] | []> {
-  //   const id_token = req.cookies['id_token'];
-
-  //   const orders = await this.ordersService.getOrders(id_token);
-  //   return orders;
-  // }
-  // /**
-  //  * Obtener una orden específica por su ID.
-  //  *
-  //  * @param id - El ID de la orden que se desea obtener.
-  //  * @param req - El objeto de solicitud HTTP que contiene las cookies.
-  //  * @returns Los detalles de la orden especificada si se encuentra.
-  //  *
-  //  * Este endpoint devuelve los detalles de una orden específica identificada por su ID.
-  //  * El `id_token` se extrae de las cookies para autenticar la solicitud.
-  //  */
-  // // @Get(':id')
-  // // async getOrderById(@Param('id') order: string, @Req() req: Request): Promise<Order | []> {
-  // //   const id_token = req.cookies['id_token'];
-  // //   const orderFound = this.ordersService.getOrderById(id_token, order);
-  // //   return orderFound;
-  // // }
-
-  // /**
-  //  * Crear una nueva orden.
-  //  *
-  //  * @param order - Los datos para la nueva orden.
-  //  * @returns El objeto de la orden creada.
-  //  *
-  //  * Este endpoint crea una nueva orden con los datos proporcionados.
-  //  * Los datos deben coincidir con la estructura definida en `CreateOrderDto`.
-  //  */
-  // @Post()
-  // async createOrder(@Body() order: CreateOrderDto, @Req() req: Request): Promise<Order | []> {
-  //   const userId = req.cookies['id_token'];
-
-  //   const orderCreated = await this.ordersService.createOrder(userId, order as Order);
-  //   return orderCreated;
-  // }
-
-  // /**
-  //  * Eliminar una orden por su ID.
-  //  *
-  //  * @param orderId - El ID de la orden que se desea eliminar.
-  //  * @param req - El objeto de solicitud HTTP que contiene las cookies.
-  //  * @returns Un mensaje de confirmación o los detalles de la orden eliminada.
-  //  *
-  //  * Este endpoint elimina una orden identificada por su ID.
-  //  * El `id_token` se extrae de las cookies para autenticar la solicitud.
-  //  */
-  // @Delete(':id')
-  // async deleteOrderById(@Param('id') orderId: number, @Req() req: Request): Promise<Order | []> {
-  //   const id_token = req.cookies['id_token'];
-  //   return this.ordersService.deleteOrderById(id_token, orderId);
-  // }
 }
