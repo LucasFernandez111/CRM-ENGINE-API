8ea2b7b2ecd5457991481c1996b1ca7570c38080
LucasFernandez111 <flucasyoel@outlook.es>
2024-10-10 15:38:38 -0300

    feat(controller): :sparkles: New endpoint for get pdf document

--- src/modules/orders/controllers/orders.controller.ts
+++ src/modules/orders/controllers/orders.controller.ts
@@ -1,14 +1,46 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  Res,
+  StreamableFile,
+  UseGuards,
+} from '@nestjs/common';
 import { Order } from '../../../schemas/orders.schema';
 import { UpdateOrderDto, CreateOrderDto } from '../dto';
 import { OrdersService } from '../services/orders.service';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth-guard/jwt-auth.guard';
 import { UserExistsGuard } from 'src/modules/users/guards/user-exists.guard';
 import { PayloadToken } from 'src/modules/auth/interfaces/payload-token.interface';
+import { GeneratePDFService } from 'src/modules/generate/generate.service';
+import * as PDFDocument from 'pdfkit';
+import * as fs from 'fs';
+import { Response } from 'express';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    private readonly ordersService: OrdersService,
+    private readonly generateService: GeneratePDFService,
+  ) {}
+
+  @Get('pdf')
+  getOrderPDF(@Res() res: Response) {
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename="report.pdf"',
+    });
+
+    const docPDF = this.generateService.getReportPDF(res);
+
+    docPDF.end();
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard, UserExistsGuard)
