9d6966823524994e26be8cd9f18135d6863b98ed
Lucas <flucasyoel@gmail.com>
2024-10-11 12:21:21 -0300

    refactor(module): :fire: Delete import

--- src/modules/generate/generate.module.ts
+++ src/modules/generate/generate.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { GenerateController } from './generate.controller';
 import { GeneratePDFService } from './generate.service';
 
 @Module({
-  controllers: [GenerateController],
   providers: [GeneratePDFService],
   exports: [GeneratePDFService],
 })
