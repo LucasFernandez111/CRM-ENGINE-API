7e730f75cbf921ca55d86120dc02e56a446b6683
Lucas <flucasyoel@gmail.com>
2024-11-15 15:54:12 -0300

    refactor: 🔄 Remove OrdersStatisticsController from OrdersModule

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -7,7 +7,6 @@ import { OrderRepository } from './services/order-repository/order-repository.se
 import { SalesStatisticsService } from './services/sales-statistics/sales-statistics.service';
 import { OrdersService } from './services/orders.service';
 import { DateFilterService } from './services/date-filter/date-filter.service';
-import { OrdersStatisticsController } from './controllers/orders-statistics.controller';
 import { AuthModule } from '../auth/auth.module';
 import { GenerateModule } from '../generate/generate.module';
 import { StatisticsOrderService } from './services/statistics-order.service';
@@ -19,7 +18,7 @@ import { StatisticsOrderService } from './services/statistics-order.service';
     UsersModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
-  controllers: [OrdersController, OrdersStatisticsController],
+  controllers: [OrdersController],
   providers: [OrdersService, OrderRepository, DateFilterService, SalesStatisticsService, StatisticsOrderService],
   exports: [OrdersService],
 })
