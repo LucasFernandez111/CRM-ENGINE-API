33392013228ff7d14caf3fcf17d3eb9fa929a7ae
Lucas <flucasyoel@gmail.com>
2024-11-15 16:15:55 -0300

    refactor: ✏️ Improve sheet-related routes with dynamic parameters and error handling

--- src/modules/sheets/controllers/sheets/sheets.controller.ts
+++ src/modules/sheets/controllers/sheets/sheets.controller.ts
@@ -1,31 +1,56 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards, Param, Body, Post, Put } from '@nestjs/common';
 import { SheetService } from '../../services/sheets/sheet.service';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth-guard/jwt-auth.guard';
 import { PayloadToken } from 'src/modules/auth/interfaces/payload-token.interface';
+import ErrorManager from 'src/config/error.manager';
 
 @Controller('sheets')
 export class SheetsController {
   constructor(private readonly sheetService: SheetService) {}
 
-  @Get('products')
+  @Get('products/:sheetId')
   @UseGuards(JwtAuthGuard)
-  async getProducts(@Req() req) {
+  async getProducts(@Req() req, @Param('sheetId') sheetId: string) {
     const { sub: id_token, accessToken: access_token }: PayloadToken = req.user;
 
+    if (!sheetId) {
+      throw ErrorManager.createSignatureError(
+        new ErrorManager({ type: 'UNAUTHORIZED', message: 'ID sheet not found' }).message,
+      );
+    }
     const ouath2Client = this.sheetService.getOauth2Client({ id_token, access_token });
-
     return {
-      products: await this.sheetService.getSheetProducts(ouath2Client, '1qay0Xei1JZnILrRF8cNXmwyiL6X6JrPrUbFvOJgzXMk'),
+      products: await this.sheetService.getSheetProducts(ouath2Client, sheetId),
     };
   }
 
-  @Get('products/categories')
+  @Put('products/:sheetId/:range')
   @UseGuards(JwtAuthGuard)
-  async getCategory(@Req() req) {
-    const { sub: id_token, accessToken: access_token } = req.user;
+  async getProduct(@Req() req, @Body() updatedRows, @Param('sheetId') sheetId: string, @Param('range') range: string) {
+    const { sub: id_token, accessToken: access_token }: PayloadToken = req.user;
+
+    if (!sheetId) {
+      throw ErrorManager.createSignatureError(
+        new ErrorManager({ type: 'UNAUTHORIZED', message: 'ID sheet not found' }).message,
+      );
+    }
     const ouath2Client = this.sheetService.getOauth2Client({ id_token, access_token });
 
-    const sheetId = '1qay0Xei1JZnILrRF8cNXmwyiL6X6JrPrUbFvOJgzXMk';
+    return await this.sheetService.updateRowsSheet(ouath2Client, range, sheetId, updatedRows);
+  }
+
+  @Get('products/categories/:sheetId')
+  @UseGuards(JwtAuthGuard)
+  async getCategory(@Req() req, @Param('sheetId') sheetId: string) {
+    const { sub: id_token, accessToken: access_token } = req.user;
+
+    const ouath2Client = this.sheetService.getOauth2Client({ id_token, access_token });
+
+    if (!sheetId) {
+      throw ErrorManager.createSignatureError(
+        new ErrorManager({ type: 'UNAUTHORIZED', message: 'ID sheet not found' }).message,
+      );
+    }
 
     const categories = await this.sheetService.getCategories(ouath2Client, sheetId);
     const subcategories = await this.sheetService.getSubcategories(ouath2Client, sheetId);
