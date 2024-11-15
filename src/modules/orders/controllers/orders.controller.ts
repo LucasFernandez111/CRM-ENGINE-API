40becb09a3fe1106f29054b90c556d71502297ba
Lucas <flucasyoel@gmail.com>
2024-11-15 15:52:10 -0300

    feat: ✨ Add sales and top-order statistics endpoints to OrdersController

--- src/modules/orders/controllers/orders.controller.ts
+++ src/modules/orders/controllers/orders.controller.ts
@@ -1,33 +1,23 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-  Req,
-  Res,
-  StreamableFile,
-  UseGuards,
-} from '@nestjs/common';
-import { Order } from '../../../schemas/orders.schema';
-import { UpdateOrderDto, CreateOrderDto } from '../dto';
-import { OrdersService } from '../services/orders.service';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
+import ErrorManager from 'src/config/error.manager';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth-guard/jwt-auth.guard';
-import { UserExistsGuard } from 'src/modules/users/guards/user-exists.guard';
 import { PayloadToken } from 'src/modules/auth/interfaces/payload-token.interface';
 import { GeneratePDFService } from 'src/modules/generate/generate.service';
-import * as PDFDocument from 'pdfkit';
-import * as fs from 'fs';
-import { Response } from 'express';
+import { UserExistsGuard } from 'src/modules/users/guards/user-exists.guard';
+import { Order } from '../../../schemas/orders.schema';
+import { CreateOrderDto, UpdateOrderDto } from '../dto';
+import { OrdersService } from '../services/orders.service';
+import { SalesStatisticsService } from '../services/sales-statistics/sales-statistics.service';
+import { StatisticsOrderService } from '../services/statistics-order.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(
     private readonly ordersService: OrdersService,
     private readonly generateService: GeneratePDFService,
+    private readonly statisticsOrderService: StatisticsOrderService,
+    private readonly statisticsSalesService: SalesStatisticsService,
   ) {}
 
   @Get('pdf/:id')
@@ -66,5 +56,48 @@ export class OrdersController {
   @Put(':id')
   async updateOrder(@Param('id') id: string, @Body() order: UpdateOrderDto): Promise<Order> {
     return await this.ordersService.updateOrder(id, order);
+  }
+
+  @Get('statistics/sales')
+  @UseGuards(JwtAuthGuard, UserExistsGuard)
+  async getSummarySales(@Req() req) {
+    const { sub: userId, sheetId }: PayloadToken = req.user;
+    const initialDate = new Date();
+
+    if (!sheetId) {
+      throw ErrorManager.createSignatureError(
+        new ErrorManager({ type: 'UNAUTHORIZED', message: 'ID sheet not found' }).message,
+      );
+    }
+    return {
+      sales: {
+        total: await this.statisticsSalesService.getTotalSales(userId),
+        current: await this.statisticsSalesService.getTotalSalesSummary(userId, initialDate),
+        periodSales: {
+          salesMonth: await this.statisticsSalesService.getSalesByMonth(userId, initialDate),
+          salesWeek: '',
+          salesDay: '',
+          salesYear: '',
+        },
+      },
+    };
+  }
+
+  @Get('statistics/top-order')
+  @UseGuards(JwtAuthGuard, UserExistsGuard)
+  async getTopOrder(@Req() req) {
+    const { sub: userId, sheetId }: PayloadToken = req.user;
+    if (!sheetId) {
+      throw ErrorManager.createSignatureError(
+        new ErrorManager({ type: 'UNAUTHORIZED', message: 'ID sheet not found' }).message,
+      );
+    }
+
+    return {
+      topOrder: {
+        period: await this.statisticsOrderService.getInfoTopOrder(userId),
+        today: await this.statisticsOrderService.getInfoTopOrderToday(userId),
+      },
+    };
   }
 }
